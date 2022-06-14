2f8d6f423b615ec24ee4b9c275a475ad0455a92c
Brandon Hach <58790036+bamboosmoothies@users.noreply.github.com>
2022-06-14 12:46:27 -0400

    commit

--- src/Lesson25_HashMap/WorkingWithHashMap.java
+++ src/Lesson25_HashMap/WorkingWithHashMap.java
@@ -19,15 +19,24 @@ public class WorkingWithHashMap {
         countries.put("Russia", "Moscow");
         countries.put("China", "Beijing");
 
+        // REMOVES KEY
         // countries.remove("USA");
+        // RETURNS VALUE
         // System.out.println(countries.get("Russia"));
+        // CLEARS HASHMAP (EMPTY)
         // countries.clear();
+        // RETURNS SIZE
         // System.out.println(countries.size());
+        // REPLACES VALUE OF KEY
         // countries.replace("USA", "Detroit");
+        // RETURNS BOOLEAN ON KEY
         // System.out.println(countries.containsKey("England"));
+        // RETURNS BOOLEAN ON VALUE
         // System.out.println(countries.containsValue("Beijing"));
+        // RETURNS HASHMAP
         // System.out.println(countries);
 
+        // ENHANCED FOR LOOP FOR PRINTING HASHMAP IN FORMAT
         for (String i : countries.keySet()) {
             System.out.print(i + "\t" + "= ");
             System.out.println(countries.get(i));
