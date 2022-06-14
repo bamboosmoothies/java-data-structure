e467d83f1d72ae4f7f3c00f767436147d48e8c32
Brandon Hach <58790036+bamboosmoothies@users.noreply.github.com>
2022-06-14 12:14:24 -0400

    commit

---
+++ src/Lesson25_HashMap/WorkingWithHashMap.java
@@ -0,0 +1,37 @@
+package Lesson25_HashMap;
+
+import java.util.HashMap;
+
+public class WorkingWithHashMap {
+    /**
+     * HashMap implements the Map interface (need import)
+     * HashMap is similar to ArrayList, but with key-value pairs
+     * store objects, need to use Wrapper Class (Integer, Boolean, Double, etc...)
+     * ex: (name, email), (username, userID), (country, capital)
+     * 
+     */
+    public static void main(String[] args) {
+        HashMap<String, String> countries = new HashMap<String, String>();
+
+        // add a key & value
+        countries.put("USA", "Washington DC");
+        countries.put("India", "New Delhi");
+        countries.put("Russia", "Moscow");
+        countries.put("China", "Beijing");
+
+        // countries.remove("USA");
+        // System.out.println(countries.get("Russia"));
+        // countries.clear();
+        // System.out.println(countries.size());
+        // countries.replace("USA", "Detroit");
+        // System.out.println(countries.containsKey("England"));
+        // System.out.println(countries.containsValue("Beijing"));
+        // System.out.println(countries);
+
+        for (String i : countries.keySet()) {
+            System.out.print(i + "\t" + "= ");
+            System.out.println(countries.get(i));
+        }
+    }
+
+}
